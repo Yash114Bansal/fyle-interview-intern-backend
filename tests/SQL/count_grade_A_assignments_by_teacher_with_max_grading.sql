28c21e21077389f116c8a6e5f885052018ac3858
yash114bansal <yash114bansal@gmail.com>
2024-05-15 02:10:40 +0530

    add SQL queries

--- tests/SQL/count_grade_A_assignments_by_teacher_with_max_grading.sql
+++ tests/SQL/count_grade_A_assignments_by_teacher_with_max_grading.sql
@@ -1,1 +1,10 @@
 -- Write query to find the number of grade A's given by the teacher who has graded the most assignments
+SELECT assignment_count
+FROM (
+    SELECT teacher_id, COUNT(*) AS assignment_count
+    FROM assignments
+    WHERE grade = 'A'
+    GROUP BY teacher_id
+    ORDER BY assignment_count DESC
+    LIMIT 1
+) AS count_grade_A_assignments_by_teacher_with_max_grading
--- tests/SQL/number_of_graded_assignments_for_each_student.sql
+++ tests/SQL/number_of_graded_assignments_for_each_student.sql
@@ -1,1 +1,5 @@
 -- Write query to get number of graded assignments for each student:
+SELECT student_id, COUNT(*) AS number_of_graded_assignments_for_each_student
+FROM assignments
+WHERE state = 'GRADED'
+GROUP BY student_id;
